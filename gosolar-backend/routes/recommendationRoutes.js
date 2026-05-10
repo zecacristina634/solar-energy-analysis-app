1c5e94128ebf4129d7f76ddffa01a867be52fac1
zecacristina634 <zecacristina634@gamil.com>
2026-05-10 19:19:47 +0300

    commit recommendations+report controllers+routes

--- gosolar-backend/routes/recommendationRoutes.js
+++ gosolar-backend/routes/recommendationRoutes.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const recommendationController = require('../controllers/recommendationController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.get('/unread', authMiddleware, recommendationController.getUnreadRecommendations);
+router.get('/type/:type', authMiddleware, recommendationController.getRecommendationsByType);
+router.get('/system/:systemId', authMiddleware, recommendationController.getRecommendationsBySystem);
+router.post('/generate/:systemId', authMiddleware, recommendationController.generateRecommendations);
+router.patch('/read-all', authMiddleware, recommendationController.markAllAsRead);
+router.delete('/old', authMiddleware, recommendationController.deleteOldRecommendations);
+router.get('/', authMiddleware, recommendationController.getRecommendations);
+router.get('/:id', authMiddleware, recommendationController.getRecommendationById);
+router.patch('/:id/read', authMiddleware, recommendationController.markAsRead);
 
 module.exports = router;
