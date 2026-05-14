f094b553cf467373a704b517388e338a3a597400
zecacristina634 <zecacristina634@gamil.com>
2026-05-14 12:28:08 +0300

    commit forecast controller + routes

--- gosolar-backend/routes/forecastRoutes.js
+++ gosolar-backend/routes/forecastRoutes.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const forecastController = require('../controllers/forecastController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.post('/fetch/:systemId', authMiddleware, forecastController.fetchForecast);
+router.get('/:systemId', authMiddleware, forecastController.getWeeklyForecast);
+router.get('/:systemId/:date', authMiddleware, forecastController.getForecastByDate);
+router.delete('/:systemId/past', authMiddleware, forecastController.deletePastForecasts);
 
 module.exports = router;
